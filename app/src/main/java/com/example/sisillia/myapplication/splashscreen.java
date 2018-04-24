9529640cd034814cc92d4b32400b7d497593d91d
Sisillia <sisilliatricahyani@yahoo.com>
2018-04-24 23:03:25 +0800

    splash-login commit

---
+++ app/src/main/java/com/example/sisillia/myapplication/splashscreen.java
@@ -0,0 +1,32 @@
+package com.example.sisillia.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class splashscreen extends Activity {
+    TextView tvSplash;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+
+        tvSplash = (TextView) findViewById(R.id.tvSplash);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginForm.class));
+                finish();
+            }
+        }, 5000L); //3000 L = 3 detik
+    }
+}
